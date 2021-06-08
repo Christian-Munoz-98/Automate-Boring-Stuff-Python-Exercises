c9d9f6b0567255ea3c2d5d4a9609fc225bb5e687
Christian Muñoz <chgeovany.15@gmail.com>
2021-06-08 18:44:22 -0500

    Cambio de tiempo

--- conway.py
+++ conway.py
@@ -64,4 +64,4 @@ numNeighbors == 3):
             else:
                 # Everything else dies or stays dead:
                 nextCells[x][y] = ' '
-    time.sleep(1) # Add a 1-second pause to reduce flickering.
+    time.sleep(0.00001) # Add a 1-second pause to reduce flickering.
