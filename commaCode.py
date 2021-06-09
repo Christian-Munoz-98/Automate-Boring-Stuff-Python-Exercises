1a1e513e1c8a3dae771d2c2e7a2404be051dce51
Christian Muñoz <chgeovany.15@gmail.com>
2021-06-09 16:56:04 -0500

    Comma separation

---
+++ commaCode.py
@@ -0,0 +1,26 @@
+def separation(my_list):
+    concatenate = ''
+
+    for i in my_list[:len(my_list)-1]:
+        concatenate += i + ',' + ' '
+
+    concatenate += 'and ' + my_list[len(my_list)-1]
+    return concatenate
+    
+def run():
+
+    my_list = []
+    
+    for i in range(5):
+        element = input("Add an element to this list----> ")
+        my_list.append(element)
+    
+    if len(my_list) == 0:
+        print('Error! empty list!')
+    else:
+        print(separation(my_list))
+
+if __name__ == '__main__':
+    run()
+
+
