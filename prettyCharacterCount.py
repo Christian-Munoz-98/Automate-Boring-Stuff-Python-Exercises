e0bc490ff1df48daa4b0a5de72c6d92067c92b9f
Christian Muñoz <chgeovany.15@gmail.com>
2021-06-11 18:48:18 -0500

    Character counts

---
+++ prettyCharacterCount.py
@@ -0,0 +1,17 @@
+import random, pprint
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', ' ', 'Z']
+
+message = ''
+
+for i in range(1000000):
+    message += random.choice(alphabet)
+
+print(message)
+
+count = {}
+
+for character in message:
+    count.setdefault(character,0)
+    count[character] += 1
+
+pprint.pprint(count)
